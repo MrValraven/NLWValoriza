2b2550f26852e686028691b2af667f2050881dfd
Tiago Costa <akatiago@gmail.com>
2021-07-25 01:19:09 +0100

    init

---
+++ src/entities/User.ts
@@ -0,0 +1,26 @@
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { v4 } from 'uuid';
+@Entity("users")
+class User {
+    @PrimaryColumn()
+    readonly id: string;
+    
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @CreateDateColumn()
+    updated_at: Date;
+}
+
+export { User };
+
+
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+
+/* 
+
+GET => Buscar uma informação
+POST => Inserir (criar) uma informação
+PUT => Alterar uma informação
+DELETE => Remover um dado
+PATCH => alterar uma informação especifica
+
+*/
+
+
+/* 
+
+Tipos de Parametros
+- Routes Params => https://localhost:3000/produtos/9473843
+- Query Params => https://localhost:3000/produtos?name=teclado&description=teclado_bom
+
+- Body Params => {
+    "name": "teclado",
+    "description": teclado bom
+}
+
+*/
+
+import express from 'express';
+import 'reflect-metadata'
+
+import "./database"
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("hehe");
+});
